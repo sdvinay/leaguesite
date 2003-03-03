5cf82cf1cda3087a95877e05662741f7df0051db
vinay <empty>
2003-03-03 08:28:45 +0000

    Fixed checkin script so that it passes proper flags to   ci (namely, the time-zone flag).

--- scripts/label.sh
+++ scripts/label.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-# $Revision: 1.4 $
-# $Date: 2003/03/01 05:18:34 $
+# $Revision: 1.5 $
+# $Date: 2003/03/03 02:53:52 $
 
 prefix="$1"
 if [ $# -lt 2 ]
@@ -10,6 +10,9 @@ then
 else
 	labelopt="-NRelease_00_$2"
 fi
+
+checkinopts="-l -zLT"
+checkoutopts="-l -zLT"
 
 if [ -d $prefix/RCS ]
 then
@@ -22,7 +25,7 @@ then
 		then
 			rcsdiff $prefix/$i
 			if [ -x $prefix/$i ] ; then exe=1;  else exe=; fi
-			ci -l $labelopt $prefix/$i
+			ci $checkinopts $labelopt $prefix/$i
 			if [ $exe ] ; then chmod u+x $prefix/$i; fi
 		fi
 	done
