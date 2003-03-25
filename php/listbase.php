e8f8157731bac74591a4fa86f600e27cfe700592
vinay <empty>
2003-03-25 07:26:14 +0000

    Added class FileBasedList

--- php/listbase.php
+++ php/listbase.php
@@ -14,10 +14,35 @@ class ListBase
 	function next()		{ return next($this->myArray); }
 	function prev()		{ return prev($this->myArray); }
 	function end()		{ return end($this->myArray); }
-	function each()		{ return each($this->myArray); }
 	function key()		{ return key($this->myArray); }
 	
 	function Count()	{ return count($this->myArray); }
+}
+
+class FileBasedList extends ListBase
+{
+	var $datafile_path; // should be set by derived class
+	var $item_class; // name of class in list; should be set by derived class
+	
+	function Generate($index_field_name)
+	{
+		$lines = @file($this->datafile_path);
+		list($dummy, $format_line) = each($lines);
+		$fields  = split(":", trim($format_line));
+		
+		while (list($dummy, $data_line) = each($lines))
+		{
+			trim ($data_line);
+			$data_as_list = split(":", $data_line);
+			$obj = new $this->item_class;
+			while (list($i, $data_item) = each($data_as_list))
+			{
+				$field_name = $fields[$i];
+				$obj->$field_name = $data_item;
+			}
+			$this->myArray[$obj->$index_field_name] = $obj;
+		}
+	}
 }
 
 ?>
