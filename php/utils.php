12112b6bfa87b6878cee5216997f8c4ed7e2ffca
vinay <empty>
2003-03-25 07:27:20 +0000

    Moved Team and Player classes to their own files

--- php/utils.php
+++ php/utils.php
@@ -1,14 +1,13 @@
 <?php
 
-# $Revision: 1.5 $
-# $Date: 2003/02/28 20:33:10 $
+# $Revision: 1.6 $
+# $Date: 2003-02-28 23:35:20-08 $
 
 // Initialization stuff
 session_start();
 umask(0000);
 
 $teamlist; // don't pre-populate this, it will be populated on first use
-$playerlist; // ditto
 $LgOptions;
 
 #read the league.txt file
@@ -36,99 +35,6 @@ function WriteLeagueOptions()
 		fwrite($fh, "$key~$value\n"); // TODO error
 	}
 	fclose($fh);
-}
-
-class Team
-{
-	var $teamnum;
-	var $teamname;
-	var $password;
-	var $ownername;
-	var $email;
-	var $ballpark;
-	var $plyrsowned;
-	var $plyrsbid;
-	var $payrollowned;
-	var $payrollbid;
-	// will add more as needed
-	
-	// order of args to ctor is consistent with teams.txt
-	function Team($in_teamnum, $in_password, $in_teamname, $in_ownername, $in_email, 
-		$in_ballpark, $in_plyrsowned, $in_plyrsbid, $in_payrollowned, $in_payrollbid)
-	{
-		$this->teamnum = $in_teamnum;
-		$this->teamname = $in_teamname;
-		$this->password = $in_password;
-		$this->ownername = $in_ownername;
-		$this->email = $in_email;
-		$this->ballpark = $in_ballpark;
-		$this->plyrsowned = $in_plyrsowned;
-		$this->plyrsbid = $in_plyrsbid;
-		$this->payrollowned = $in_payrollowned;
-		$this->payrollbid = $in_payrollbid;
-	}
-}
-
-class Player
-{
-	var $playernum;
-	var $playername;
-	var $status;
-	var $team;
-	var $salary;
-	
-	function Player($in_playernum, $in_playername, $in_status, $in_team, $in_salary)
-	{
-		$this->playernum = $in_playernum;
-		$this->playername = $in_playername;
-		$this->status = $in_status;
-		$this->team = $in_team;
-		$this->salary = $in_salary;
-	}
-}
-
-function ReadInTeamList()
-{
-	global $teamlist;
-	$lines = file("$$_data_loc$$/teams.txt");
-	foreach ($lines as $teamline)
-	{
-		trim ($teamline);
-		$teamaslist = split(":", $teamline);
-		$this_team = new Team($teamaslist[0],$teamaslist[1],$teamaslist[2],$teamaslist[3],
-			$teamaslist[4],$teamaslist[5],$teamaslist[6],$teamaslist[7],$teamaslist[8],$teamaslist[9]);
-		$teamlist[$this_team->teamnum] = $this_team;
-	}
-}
-
-function ReadInPlayerList()
-{
-	global $playerlist;
-	$lines = file("$$_data_loc$$/stat.txt");
-	foreach ($lines as $playerline)
-	{
-		trim ($playerline);
-		$playeraslist = split(":", $playerline);
-		$this_player = new Player($playeraslist[0],$playeraslist[1],$playeraslist[2],$playeraslist[3],$playeraslist[4]);
-		if ($this_player->status < 9)
-			$playerlist[$this_player->playernum] = $this_player;
-	}
-}
-
-function GetTeamName($teamnum)
-{
-	global $teamlist;
-	if (!($teamlist))
-		ReadInTeamList();
-	return $teamlist[$teamnum]->teamname;
-}
-
-function GetPlayerName($playernum)
-{
-	global $playerlist;
-	if (!($playerlist))
-		ReadInPlayerList();
-	return $playerlist[$playernum]->playername;
 }
 
 function RecordUserAgent()
