24c57e69140b8ad23d6aab158d83782108686e28
vinay <empty>
2003-04-09 00:56:49 +0000

    *** empty log message ***

--- php/expenselist.php
+++ php/expenselist.php
@@ -1,7 +1,7 @@
 <?
 
-# $Revision$
-# $Date$
+# $Revision: 1.1 $
+# $Date: 2003-04-02 13:40:25-08 $
 
 require_once("utils.php");
 require_once("listbase.php");
@@ -15,10 +15,22 @@ class Expense
 	var $type;
 	var $amount;
 	var $label;
+	
+	function Populate($expnum, $teamnum, $year, $type, $amount, $label)
+	{
+		$this->expnum = $expnum;
+		$this->teamnum = $teamnum;
+		$this->year = $year;
+		$this->type = $type;
+		$this->amount = $amount;
+		$this->label = $label;
+	}
 }
 
 class ExpenseList extends FileBasedList
 {
+	var $maxnum;
+	
 	function ExpenseList()
 	{
 		$this->datafile_path = "$$_data_loc$$/expenses.txt";
@@ -28,16 +40,26 @@ class ExpenseList extends FileBasedList
 	function Generate()
 	{
 		parent::Generate("expnum");	
+		list($this->maxnum, $dummy) = $this->end();
+		$this->reset();
 	}
 	
 	function GenerateWithFilter($filter)
 	{
 		parent::GenerateWithIndexAndFilter("expnum", $filter);
+		list($this->maxnum, $dummy) = $this->end();
+		$this->reset();
 	}
 
 	function GetExpense($expnum)
 	{
 		return $this->myArray[$expnum];	
+	}
+	
+	// returns the Expnum one greater than the current max
+	function GetNewExpnum()
+	{
+		return ++$this->maxnum;
 	}
 }
 
