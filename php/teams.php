192fe7c31d89f9079608b963875498a320fd3f89
vinay <empty>
2003-05-12 23:01:06 +0000

    Added $num_cols variable so that I can more easily modify the   size/layout of the main table. Added column for non-salary expenses

--- php/teams.php
+++ php/teams.php
@@ -1,11 +1,12 @@
 <?php
 
-// $Revision$
-// $Date$
+// $Revision: 1.1 $
+// $Date: 2003-04-08 17:58:03-07 $
 
 require_once("$$_php_loc$$/organization.php");
 require_once("utils.php");
 require_once("teamlist.php");
+require_once("expenselist.php");
 
 ?>
 
@@ -25,20 +26,34 @@ $tmlist = new TeamList();
 // address (which is a concatenation of all the emails from the leaf nodes
 // on the branch)
 
+$num_cols = 9; // This is the number of columns in the main table
+
 foreach ($org->lgArray as $lgObj)
 {
 ?>
-	<tr><th colspan=8 height=50><a href="mailto:<?=$lgObj->email?>"><big +2><?= $lgObj->name ?></big></a></th></tr>
+	<tr><th colspan=<?=$num_cols?> height=50><a href="mailto:<?=$lgObj->email?>"><big +2><?= $lgObj->name ?></big></a></th></tr>
 <?
 	foreach ($lgObj->divArray as $divObj)
 	{
 ?>
-		<tr><th colspan=8><a href="mailto:<?=$divObj->email?>"><?=$divObj->name?></a></th></tr>
-		<tr><th>#</th><th>Team Name</th><th>Stadium</th><th>Owner</th><th>#</th><th>Spent</th><th>Bid</th><th>Remaining</th></tr>
+		<tr><th colspan=<?=$num_cols?>><a href="mailto:<?=$divObj->email?>"><?=$divObj->name?></a></th></tr>
+		<tr><th>#</th><th>Team Name</th><th>Stadium</th><th>Owner</th><th>#</th><th>Salaries</th><th>Bids</th><th>Exp</th><th>Remaining</th></tr>
 <?
 		foreach ($divObj->teamNumArray as $teamnum)
 		{
 			$tmObj = $tmlist->GetTeam($teamnum);
+			
+			// Total up the expenses 
+			// TODO note that non-player expenses are inconsistent with salaries, in that
+			//   the total is not stored in the teams data file
+			$expenselist = new ExpenseList(); // TODO re-use the expenselist
+			$expenselist->Generate(new SimpleFilter("teamnum", $teamnum));
+			$exp_total = 0;
+			while (list($dummy, $exp) = $expenselist->each())
+			{
+				$exp_total += $exp->amount;
+			}
+			
 ?>		
 			<tr>
 				<td><?=$teamnum?></td>
@@ -48,7 +63,8 @@ foreach ($org->lgArray as $lgObj)
 				<td align=right><?=$tmObj->plyrsowned?></td>
 				<td align=right><?=$tmObj->payrollowned?></td>
 				<td align=right><?=$tmObj->payrollbid?></td>
-				<td align=right><?= $LgOptions["salarycap"]-$tmObj->payrollowned-$tmObj->payrollbid ?></td>
+				<td align=right><?=$exp_total?></td>
+				<td align=right><?= $LgOptions["salarycap"]-$tmObj->payrollowned-$tmObj->payrollbid-$exp_total ?></td>
 			</tr>
 <?
 		} // foreach ($divObj->teamNumArray as $teamnum)
